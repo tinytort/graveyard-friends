ffdcb62f00f9c72af7caa14000034734a46c46c9
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-20 23:36:43 -0700

    allow custom ghostFriend color via attribute, add slot for eyes

--- Graveyard.js
+++ Graveyard.js
@@ -1,19 +1,20 @@
-
 class Graveyard extends HTMLElement {
+
     constructor () {
         super();
         
         const template = document.querySelector( 'template' );
         this.root = this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild( template.content.cloneNode(true) );
+        this.shadowRoot.appendChild( template.content.cloneNode( true ) );
+        this.ghostFriend = this.root.querySelector( '.ghost' );
         
-        this.addEventListener( 'click', this.toggleGhostFriend );
+        this.ghostFriend.style.backgroundColor = this.getAttribute( 'color' );
 
+        this.addEventListener( 'click', this.toggleGhostFriend );       
     }
 
     toggleGhostFriend ( e ) {
-        const ghost = this.root.querySelector( '.ghost' );
-        ghost.classList.toggle( 'haunt' );
+        this.ghostFriend.classList.toggle( 'haunt' );
     }
 }
 
