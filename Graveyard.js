b3e4046c93e6cdc3c5d376618b5846cea681582f
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-25 16:40:15 -0700

    add numbered step comments

--- Graveyard.js
+++ Graveyard.js
@@ -1,15 +1,23 @@
+// Step 2. Create your component and extend the HTMLElement class
 class Graveyard extends HTMLElement {
 
     constructor () {
         super();
         
+        // get the template we created
         const template = document.querySelector( 'template' );
-        this.root = this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild( template.content.cloneNode( true ) );
-        this.ghostFriend = this.root.querySelector( '.ghost' );
-        
-        this.ghostFriend.style.backgroundColor = this.getAttribute( 'color' );
 
+        // create a root property that references our element's shadow root
+        // "open" our shadow root so we can build in it
+        this.root = this.attachShadow({ mode: 'open' });
+
+        // copy our template and append it to our elements shadow root
+        this.shadowRoot.appendChild( template.content.cloneNode( true ) );
+        
+        // create a ghostFriend property that references our template's div.ghost
+        this.ghostFriend = this.root.querySelector( '.ghost' );
+
+        // attach our click listener!
         this.addEventListener( 'click', this.toggleGhostFriend );       
     }
 
@@ -18,4 +26,5 @@ class Graveyard extends HTMLElement {
     }
 }
 
+// define our custom element with our window
 window.customElements.define( 'grave-yard', Graveyard );
--- README.md
+++ README.md
@@ -1,22 +1,5 @@
 # Graveyard component
 
-For JS Study Night October 2017.
+For [JS Study Night October 2017](https://github.com/wwcodeportland/study-nights/blob/master/javascript/past-study-nights/10-2017.md).
 
 <img src="ghost-components.gif"></img>
-
-### Examples
-- [hamburger buttons](http://kcmr.github.io/transburger-icon/components/transburger-icon/demo/)
-
-### Resources
-- [webcomponents.github.io] (http://webcomponents.github.io/)
-- [Google](https://developers.google.com/web/fundamentals/web-components/customelements)
-- [CSS Tricks](https://css-tricks.com/modular-future-web-components/)
-- [MDN](https://developer.mozilla.org/en-US/docs/Web/Web_Components/Custom_Elements)
-- [Tutorial by Trent Willis](https://pretty-okay.com/2017/10/14/intro-web-components.html)
-
-### Steps
-1. Creating an html template
-    - adding CSS
-2. Creating the JS custom element 
-3. Using the custom element
-4. Publishing to NPM for a CDN
